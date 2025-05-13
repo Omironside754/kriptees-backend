40bc8631958a1f39947511128b7978117f7a74e3
Hariom Rajput <142091655+Omironside754@users.noreply.github.com>
2025-05-13 20:26:04 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,29 +1,23 @@
-const nodeMailer = require("nodemailer");
-// options are pass as arg from userController
+const nodemailer = require("nodemailer");
+
 const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    service: process.env.SMTP_SERVICE,
+    auth: {
+      user: process.env.SMTP_MAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
 
-    const transporter = nodeMailer.createTransport({
-          //Simple Mail Transfer Protocol (SMTP) (write any thing but that make sense. its env fi)
-        host: process.env.SMTP_HOST, // "smtp.gmail.com"
-        port: process.env.SMTP_PORT,  // 465,
-        service: process.env.SMTP_SERVICE, // gmail
-        auth: {
-            // this our email and pass wich one we use for sending mail
-            user: process.env.SMTP_MAIL,
-            pass: process.env.SMTP_PASSWORD,
-        },
-    });
+  const mailOptions = {
+    from: `Kriptees <${process.env.SMTP_MAIL}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
 
-
-    const mailOptions = {
-        from: process.env.SMPT_MAIL,
-        to: options.email,
-        subject: options.subject,
-        text: options.message, 
-    };
-
-    await transporter.sendMail(mailOptions);
-
-   
-}
-module.exports = sendEmail;
+  await transporter.sendMail(mailOptions);
+};
+module.exports = sendEmail; 
